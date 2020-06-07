f5d137063dfe61f86504377d0ea0b1281d85ceb6
吕坤 <kunlv@tuputech.com>
2020-06-07 11:19:50 +0800

    add torch transform test

--- test/demo.py
+++ test/demo.py
@@ -3,9 +3,14 @@ import warnings
 
 import numpy as np
 import paddle.fluid as fluid
-from paddle.fluid.dygraph import Conv2D, BatchNorm, Linear
-from paddletools.logger import logger
-from paddletools.checkpoints import static2dynamic, dynamic2static
+import torch
+import torch.nn.functional as F
+from paddle.fluid.dygraph import BatchNorm, Conv2D, Linear
+from paddletools import logger
+from paddletools.checkpoints import (dynamic2static, static2dynamic,
+                                     torch2dynamic)
+from torch import nn
+from torch.nn import BatchNorm2d, Conv2d, ReLU
 
 place = fluid.CPUPlace()
 logger.log_to_file("test.log")
@@ -36,8 +41,8 @@ def build_static_network(save_params=False, load_pretrain=None):
         f = fluid.layers.reshape(bn, [batch_size, -1])
         fc = fluid.layers.fc(
             input=f, size=3,
-            param_attr=fluid.ParamAttr(name='fc.weight'),
-            bias_attr=fluid.ParamAttr(name='fc.bias'))
+            param_attr=fluid.ParamAttr(name='fc.w_0'),
+            bias_attr=fluid.ParamAttr(name='fc.b_0'))
         logits = fluid.layers.softmax(fc)
     eval_prog = main_prog.clone(True)
     exe.run(startup_prog)
@@ -80,18 +85,47 @@ class TestModel(fluid.dygraph.Layer):
         return x
 
 
-def build_dynamic_network(load_params=None, save_params=False):
+def build_dynamic_network(load_params=None, save_params=False,
+                          use_structured_name=False):
     with fluid.dygraph.guard(place):
         model = TestModel("test")
         if load_params:
             model_state_dict, _ = fluid.load_dygraph(load_params)
-            model.load_dict(model_state_dict, use_structured_name=False)
+            model.load_dict(model_state_dict, use_structured_name=use_structured_name)
         model.eval()
         d = fluid.dygraph.to_variable(reader())
         p = model(d)
         logger.info(p.numpy())
         if save_params:
             fluid.save_dygraph(model.state_dict(), "dynamic_params")
+
+
+class TorchModel(nn.Module):
+
+    def __init__(self):
+        super(TorchModel, self).__init__()
+        self.conv = Conv2d(3, 16, 3)
+        self.bn = BatchNorm2d(16)
+        self.relu = ReLU(inplace=True)
+        self.fc = nn.Linear(3136, 3)
+
+    def forward(self, x):
+        x = self.conv(x)
+        x = self.bn(x)
+        x = self.relu(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return F.softmax(x)
+
+
+def build_torch_model(save_params=True):
+    model = TorchModel()
+    model.eval()
+    d = torch.from_numpy(reader())
+    y = model(d)
+    logger.info(y.tolist())
+    if save_params:
+        torch.save(model.state_dict(), "torch_model.params")
 
 
 if __name__ == "__main__":
@@ -108,3 +142,11 @@ if __name__ == "__main__":
     logger.info(">>> read dynamic params & build static network...")
     dynamic2static("dynamic_params", "static_params")
     build_static_network(load_pretrain="static_params")
+
+    print("\n<========================>\n")
+
+    logger.info(">>> build torch model & save params...")
+    build_torch_model(save_params=True)
+    logger.info(">>> read torch params & build dynamic network...")
+    torch2dynamic("torch_model.params", "from_torch")
+    build_dynamic_network(load_params="from_torch", use_structured_name=True)
